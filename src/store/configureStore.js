c993ee70d116cd72f1a9630baf1e029d0a79cbd0
Mats Nord <mnd@qlik.com>
2017-03-19 22:06:24 +0100

    Production Builds

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,6 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-
-export default function configureStore( initialState ){
-	return createStore (
-		rootReducer,
-		initialState,
-		applyMiddleware( thunk, reduxImmutableStateInvariant() )
-	);
+// Dynamic module loading is not supported in ES6, using require
+if ( process.env.NODE_ENV === 'production') {
+	module.exports = require('./configureStore.prod');
+} else {
+	module.exports = require('./configureStore.dev');
 }
